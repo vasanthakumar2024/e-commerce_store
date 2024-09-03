7f4f0a868ca2f2ae39808524eb1bba2e9c6dd215
vasanthakumar2024 <vasanthakumarm@anjaconline.org>
2024-09-03 12:31:26 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import tshirt from "./img/1.jpg";
+import watch from "./img/2.jpg";
+import shoes from "./img/3.jpg";
+
+const products = [
+  { id: 1, name: "tshirt", price: 40, image: tshirt },
+  { id: 2, name: "watch", price: 20, image: watch },
+  { id: 3, name: "shoes", price: 10, image: shoes },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [cart, setCart] = useState([]);
+  const addToCart = (productId) => {
+    const selectedProduct = products.find(
+      (product) => product.id === productId
+    );
 
+    if (selectedProduct) {
+      setCart([...cart, selectedProduct]);
+    }
+  };
+
+const checkout = () => {
+  alert("Thank you for your parchase!");
+  setCart([]);
+};
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <h1>E-Commerce Store</h1>
+    </header>
+    <section className="App-products">
+      {products.map((product) => (
+        <div key={product.id} className="App-product">
+          <img src={product.imge} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>${product.price}</p>
+          <button onClick={() => addToCart(product.id)}>Add to cart</button>
+        </div>
+      ))}
+    </section>
+    <section className="App-cart">
+      <h2>Shopping cart</h2>
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>{item.name}-${item.price}</li>
+        ))}
+      </ul>
+      <button onClick={checkout}>Checkout</button>
+    </section>
+  </div>
+);
+}
 export default App;
